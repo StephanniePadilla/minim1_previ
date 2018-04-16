57e475680448a9340d1ddc5d5b347b08dd4c2ed4
steph <stephypadilla08@gmail.com>
2018-04-16 21:42:22 +0200

    PARTEII hago APP de vista

--- src/Main/View/App.java
+++ src/Main/View/App.java
@@ -1,4 +1,40 @@
 package Main.View;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
 public class App {
+    public static final String BASE_URI = "http://localhost:8080/minim1_previ/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+
+    public static HttpServer startServer() {
+        // set a resource config that scans for JAX-RS resources and providers
+        final ResourceConfig rc = new ResourceConfig().packages("main.Controller");
+
+        // set and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main( String[] args )
+    {
+        final HttpServer server = startServer();
+
+        StaticHttpHandler staticHttpHandler = new StaticHttpHandler("./web/");
+        server.getServerConfiguration().addHttpHandler(staticHttpHandler, "/");
+
+
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+
+        //ProductManagerImpl.getInstance().initializeUsers();
+    }
 }
